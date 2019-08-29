2e51afb6031466d9e011e7e88dff95564f1a5388
Talha <you@example.com>
2019-08-29 19:53:45 +0000

    Node Data structure initialized

--- algo.py
+++ algo.py
@@ -12,19 +12,19 @@ class Node:
 
     def set_parent(self, p_name):
         self.parent_name = p_name
-	
+
 	def set_children(self, child_array):
     		self.children = child_array
-	
+
 	def set_is_mandetory(self, is_mendatory):
     		self.is_mendatory = is_mendatory
-	
+
 	def set_is_alternative(self, is_alternative):
     		self.is_alternative = is_alternative
-	
+
 	def set_is_or(self, is_or):
     		self.is_or = is_or
-	
+
 	#### baqi setter bna lai python ma zaroorat ni hoti pr clarity k liye acha hota ha
 	####
 
@@ -44,39 +44,35 @@ with open("input.txt", "r") as file:
 		if line_parts[0] == "Feature Names":
 			line_parts[1] = (line_parts[1][:-1])
 			FeatureNames =  line_parts[1].split(',')
-			
+
 			for f_name in FeatureNames:
-    			node_temp = Node(name=f_name)
+				node_temp = Node(name=f_name)
 				node_dict[f_name] = node_temp
 
 		elif line_parts[0] == "Mendatory":
 			line_parts[1] = (line_parts[1][:-1])
 			Mendatory = line_parts[1].split(',')
 			for men in Mendatory:
-    			node = node_dict.get(men)
+				node = node_dict.get(men)
 				if node is not None:
-    				node.set_is_mandetory(True)
-			
+					node.is_mendatory =  True
+
 		elif line_parts[0] == "Alternative":
 			line_parts[1] = (line_parts[1][:-1])
-			try:
-				sub_line_parts = line_parts[1].split(':')
-				for part in sub_line_parts:
-					sub_line_parts = part.split(',')
-					Alternative.append(sub_line_parts)
-			except:
-				Alternative = line_parts[1].split(',')
-			print(Alternative)
+			Alternative = line_parts[1].split(',')
+			#print(Alternative)
+			for item in Alternative:
+				node = node_dict.get(item)
+				if node is not None:
+					node.set_is_alternative =  True
 		elif line_parts[0] == "OR":
 			line_parts[1] = (line_parts[1][:-1])
-			try:
-				sub_line_parts = line_parts[1].split(':')
-				for part in sub_line_parts:
-					sub_line_parts = part.split(',')
-					OR_array.append(sub_line_parts)
-			except:
-				OR_array = line_parts[1].split(',')
-			print(OR_array)
+			OR_array = line_parts[1].split(',')
+			#print(OR_array)
+			for item in OR_array:
+				node = node_dict.get(item)
+				if node is not None:
+					node.set_is_or =  True
 		elif line_parts[0] == "inclusive":
 			line_parts[1] = (line_parts[1][:-1])
 			try:
@@ -84,9 +80,22 @@ with open("input.txt", "r") as file:
 				for part in sub_line_parts:
 					sub_line_parts = part.split(',')
 					inclusive.append(sub_line_parts)
+				for items in inclusive:
+					node0 = node_dict.get(items[0])
+					if node0 is not None:
+						node0.inclusive_to = items[1]
+					node1 = node_dict.get(items[1])
+					if node1 is not None:
+						node1.inclusive_to = items[0]
 			except:
 				inclusive = line_parts[1].split(',')
-			print(inclusive)
+				node0 = node_dict.get(inclusive[0])
+				if node0 is not None:
+					node0.inclusive_to(inclusive[1])
+				node1 = node_dict.get(inclusive[1])
+				if node1 is not None:
+					node1.inclusive_to(inclusive[0])
+			#print(inclusive)
 		elif line_parts[0] == "exclusive":
 			line_parts[1] = (line_parts[1][:-1])
 			try:
@@ -94,9 +103,22 @@ with open("input.txt", "r") as file:
 				for part in sub_line_parts:
 					sub_line_parts = part.split(',')
 					exclusive.append(sub_line_parts)
+				for items in exclusive:
+					node0 = node_dict.get (items[0])
+					if node0 is not None:
+						node0.exclusive_to = items[1]
+					node1 = node_dict.get(items[1])
+					if node1 is not None:
+						node1.exclusive_to = items[0]
 			except:
 				exclusive = line_parts[1].split(',')
-			print(exclusive)
+				node0 = node_dict.get(exclusive[0])
+				if node0 is not None:
+					node0.exclusive_to(exclusive[1])
+				node1 = node_dict.get(exclusive[1])
+				if node1 is not None:
+					node1.exclusive_to = exclusive[0]
+			#print(exclusive)
 		elif line_parts[0] == "PC":
 			line_parts[1] = (line_parts[1][:-1])
 			try:
@@ -104,9 +126,24 @@ with open("input.txt", "r") as file:
 				for item in sub_line_parts:
 					parent_child = item.split(':')
 					PC[parent_child[0]] = parent_child[1].split(',')
+				#for i in range(0,len(PC)-1):
+				#	key = PC[i]
+				for keys in PC.keys():
+					for key in PC.keys():
+						if keys in PC[key]:
+							print(keys + ' My parent is ' + key)
+							parent_node = node_dict.ge(keys)
+							if parent_node is not None:
+								parent_node.parent_name = key
+						child_node = node_dict.ge(key)
+						if child_node is not None:
+							child_node.children = PC[key]
+
+
 			except:
 				parent_child = line_parts[1].split(':')
 				PC[parent_child[0]] = parent_child[1].split(',')
-			print(PC)
+
+			#print(PC)
 
 
